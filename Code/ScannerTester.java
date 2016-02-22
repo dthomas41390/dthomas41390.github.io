e56d6aa3625002ab273ad739210aaff281c6aa9b
btbam91 <n.o.brito@hotmail.com>
2016-02-21 22:33:05 -0800

    Courses text file for reading in Courses

---
+++ Code/ScannerTester.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.util.Scanner;
+import java.lang.*;
+import java.io.IOException;
+
+public class ScannerTester {
+
+    public static void main(String[] args)
+    {
+        FileReading object = new FileReading();
+        object.readFile();
+        
+    }
+    
+
+    
+    
+}
