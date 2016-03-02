799ac53b3d936605b91261c6ca306ad9e149d19b
JohnCrisanto <jcrisanto87@gmail.com>
2016-03-02 09:23:36 -0800

    Added files via upload
    Team, I have uploaded a sample code of our login. I tested it out and it works.  I have also attached a .txt file in the "documentation" folder, which I used to test this code and it worked.  It basically allows the user to enter their name and password for a total of 3 times before the program terminates.

---
+++ Code/Login.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class Login {
+
+    public static void main(String[] args) {
+        File file;
+        Scanner scannerRead, scannerInput;
+        String name, password, nameInput, passwordInput;
+        ArrayList<String> nameArrayList = new ArrayList<String>();
+        ArrayList<String> passwordArrayList = new ArrayList<String>();
+        scannerInput = new Scanner(System.in);
+
+        try {
+            file = new File("Place Location of NameAndPassword.txt here");
+            scannerRead = new Scanner(file);
+            scannerRead.useDelimiter(",");
+
+
+            while (scannerRead.hasNext()) {
+                name = scannerRead.next();
+                password = scannerRead.next();
+                nameArrayList.add(name);
+                passwordArrayList.add(password);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        }
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Please enter your name: ");
+            nameInput = scannerInput.next();
+            System.out.println("Please enter your password: ");
+            passwordInput = scannerInput.next();
+            if (nameArrayList.contains(nameInput) && passwordArrayList.contains(passwordInput)) {
+                System.out.println("Login Successful");
+                break;
+            } else {
+                System.out.println("Name and Password do not match, please try again" + "\n");
+            }
+
+        }
+
+    }
+
+}
