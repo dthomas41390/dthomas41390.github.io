30f0be74346c1528bfae3b09d3a3c2698891bd92
Albeto Bellido <AlbertoBellido@users.noreply.github.com>
2016-03-14 04:53:09 -0400

    Added files via upload

--- src/Course.java
+++ src/Course.java
@@ -1,7 +1,7 @@
 import java.util.Vector;
 
 public class Course implements Comparable<Course> {
-    
+
     //Private Instance Variables
     private String courseName;
     private String courseID;
@@ -10,15 +10,15 @@ public class Course implements Comparable<Course> {
     private int enrollmentLimit;
     private int numEnrolled;
     private String description;
-    
-    
+
+
     //Constructor
-    public Course(String courseName, 
-                  String courseID, 
-                  String startDate, 
-                  String endDate, 
+    public Course(String courseName,
+                  String courseID,
+                  String startDate,
+                  String endDate,
                   int enrollmentLimit,
-                  int numEnrolled, 
+                  int numEnrolled,
                   String description)
     {
         this.courseName = courseName;
@@ -28,101 +28,101 @@ public class Course implements Comparable<Course> {
         this.enrollmentLimit = enrollmentLimit;
         this.numEnrolled = numEnrolled;
         this.description = description;
-        
+
     }
-    
+
     /*
     *********************************
     Setter Methods
     *********************************
     */
-    
-    
+
+
     public void setCourseName(String courseName)
     {
         this.courseName = courseName;
     }
-    
+
     public void setCourseID(String courseID)
     {
         this.courseID = courseID;
     }
-    
+
     public void setStartDate(String startDate)
     {
         this.startDate = startDate;
     }
-    
+
     public void setEndDate(String endDate)
     {
         this.endDate = endDate;
     }
-    
+
     public void setEnrollmentLimit(int enrollmentLimit)
     {
         this.enrollmentLimit = enrollmentLimit;
     }
-    
+
     public void setDescription(String description)
     {
         this.description = description;
     }
-    
+
     /*
     *********************************
     Getter Methods
     *********************************
     */
-    
-    
+
+
     public String getCourseName()
     {
         return this.courseName;
     }
-    
+
     public String getCourseID()
     {
         return this.courseID;
     }
-    
+
     public String getStartDate()
     {
         return this.startDate;
     }
-    
+
     public String getEndDate()
     {
         return this.endDate;
     }
-    
+
     public int getEnrollmentLimit()
     {
         return this.enrollmentLimit;
     }
-    
+
     public int getNumEnrolled()
     {
         return this.numEnrolled;
     }
-    
+
     public String getDescription()
     {
         return this.description;
     }
-    
-  
+
+
     /*
     *********************************
     Class Methods
     *********************************
     */
-    
+
     //Increases the current enrollment number by one
     public void incrementEnrollment()
     {
         this.numEnrolled += 1;
     }
-    
+
     //Decreases the current enrollment number by one
     public void decrementEnrollment()
     {
@@ -135,7 +135,7 @@ public class Course implements Comparable<Course> {
     {
         return (this.courseName.compareTo(course.getCourseName()));
     }
-    
+
     /*
     -This method returns Course data as a String.
     -ASSIGNEE: Alberto
@@ -144,16 +144,16 @@ public class Course implements Comparable<Course> {
     public String toString()
     {
         String result = getCourseName().toUpperCase() + "\n"
-                        + "Course ID: " + getCourseID() + "\n"
-                        + "Start Date: " + getStartDate() + "\n"
+                        + "Course ID: " + getCourseID() + "  "
+                        + "Start Date: " + getStartDate() + "  "
                         + "End Date: " + getEndDate() + "\n"
                         + "Enrollment Limit: " + Integer.toString(getEnrollmentLimit()) + "\n"
                         + "Number Enrolled: " + Integer.toString(getNumEnrolled()) + "\n"
                         + "Description: " + getDescription() + "\n";
-                        
+
         return result;
     }
-    
-    
-    
+
+
+
 }
